2174f18a76388208393c5d0398d0cc9ff45ba4b2
tommyedmunds <73081204+tommyedmunds@users.noreply.github.com>
2021-08-09 16:48:58 -0500

    google oauth complete, needs to be connected with database though

---
+++ server/oauth.js
@@ -0,0 +1,28 @@
+const passport = require('passport');
+
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID:
+        '157973732471-mlqs5sviqnkd4sv1t5rduf30mvmpuk4h.apps.googleusercontent.com',
+      clientSecret: 'd3TW9l082DrgJU1DjI46q4hi',
+      callbackURL: 'http://localhost:8080/google/callback',
+    },
+    function (accessToken, refreshToken, profile, done) {
+      console.log(profile);
+      return done(null, profile);
+    }
+  )
+);
+
+module.exports = passport;
--- server/server.js
+++ server/server.js
@@ -1,15 +1,64 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
+require('./oauth');
 
 PORT = 3000;
 
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/build', express.static(path.resolve(__dirname, '../build')));
 
+app.use(
+  cookieSession({
+    name: 'hungr',
+    keys: ['keys1', 'keys2'],
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+//function validating if users are logged in
+const isLoggedIn = (req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    res.sendStatus(401);
+  }
+};
+
 app.get('/', (req, res) => {
   return res.status(200).sendFile(path.resolve(__dirname, '../index.html'));
+});
+
+//oauth related routes with corresponding middleware
+app.get('/failed', (req, res) => res.send('Login failed'));
+
+app.get('/loggedIn', isLoggedIn, (req, res) => {
+  console.log(req.locals);
+  return res.send(`Welcome ${req.user.displayName}`);
+});
+
+app.get('/google', passport.authenticate('google', { scope: ['profile'] }));
+
+app.get(
+  '/google/callback',
+  passport.authenticate('google', { failureRedirect: '/failed' }),
+  function (req, res) {
+    res.redirect('/loggedIn');
+  }
+);
+
+//direct here to destroy cookies
+app.get('/logOut', (req, res) => {
+  req.session = null;
+  req.logout();
+  res.redirect('/');
 });
 
 module.exports = app.listen(PORT, () => {
