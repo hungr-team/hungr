9b24c409776ebae6352180b1f8162e4298b358d5
jackiedouglass <jackie.douglass@icloud.com>
2021-08-11 15:58:35 -0500

    Adding likes and blocks set up, pushing up before working on favorites page and blocked page

---
+++ client/components/favorites.js
@@ -0,0 +1,7 @@
+import React, { useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import ThumbDownIcon from '@material-ui/icons/ThumbDown';
+import FavoriteCard from './favoriteCard';
+export default function Favorites() {}
