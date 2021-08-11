c42aa2e6fdfdd862c8641ee6417b36a6450e538a
theingideva <theingideva@gmail.com>
2021-08-10 21:35:02 -0400

    for hamburger menu

---
+++ client/components/Dawer.jsx
@@ -0,0 +1,144 @@
+import React from "react";
+import PropTypes from "prop-types";
+import AppBar from "@material-ui/core/AppBar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Drawer from "@material-ui/core/Drawer";
+import Hidden from "@material-ui/core/Hidden";
+import IconButton from "@material-ui/core/IconButton";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import MenuIcon from "@material-ui/icons/Menu";
+import CloseIcon from "@material-ui/icons/Close";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import VisibleItemList from "../containers/VisibleItemList";
+const drawerWidth = 240;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+  },
+  drawer: {
+    [theme.breakpoints.up("sm")]: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up("sm")]: {
+      display: "none",
+    },
+  },
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+  closeMenuButton: {
+    marginRight: "auto",
+    marginLeft: 0,
+  },
+}));
+function ResponsiveDrawer() {
+  const dummyCategories = [
+    "Hokusai",
+    "Hiroshige",
+    "Utamaro",
+    "Kuniyoshi",
+    "Yoshitoshi",
+  ];
+  const classes = useStyles();
+  const theme = useTheme();
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  function handleDrawerToggle() {
+    setMobileOpen(!mobileOpen);
+  }
+  const drawer = (
+    <div>
+      <List>
+        {dummyCategories.map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="Open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            Responsive drawer
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <nav className={classes.drawer}>
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <Hidden smUp implementation="css">
+          <Drawer
+            variant="temporary"
+            anchor={theme.direction === "rtl" ? "right" : "left"}
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+          >
+            <IconButton
+              onClick={handleDrawerToggle}
+              className={classes.closeMenuButton}
+            >
+              <CloseIcon />
+            </IconButton>
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden xsDown implementation="css">
+          <Drawer
+            className={classes.drawer}
+            variant="permanent"
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+          >
+            <div className={classes.toolbar} />
+            {drawer}
+          </Drawer>
+        </Hidden>
+      </nav>
+      <div className={classes.content}>
+        <div className={classes.toolbar} />
+        <VisibleItemList />
+      </div>
+    </div>
+  );
+}
+ResponsiveDrawer.propTypes = {
+  // Injected by the documentation to work in an iframe.
+  // You won't need it on your project.
+  container: PropTypes.object,
+};
+export default ResponsiveDrawer;
--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import SettingsList from './SettingMenu';
-import HistoryList from './HistoryList';
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import { MenuItem, Menu } from "@material-ui/core";
+import SettingsList from "./SettingMenu";
+import HistoryList from "./HistoryList";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -26,17 +27,25 @@ const useStyles = makeStyles((theme) => ({
 // import MenuItem from "@material-ui/core/MenuItem";
 
 const NavBar = () => {
-  const [user, setUser] = useState('');
-  const [setting, setSetting] = useState('');
-  const [radius, setRadius] = useState('');
-  const [foodtypes, setFoodTyes] = useState('');
-  const [preference, setPreference] = useState('');
-  const [like, setLike] = useState('');
-  const [dislike, setDislike] = useState('');
-  const [favorite, setFavorite] = useState('');
-  const [bigNoNo, setBigNoNo] = useState('');
+  const [user, setUser] = useState("default user");
+  const [menuCollapse, setMenuCollapse] = useState(false);
+  // const [setting, setSetting] = useState("");
+  // const [radius, setRadius] = useState("");
+  // const [foodtypes, setFoodTyes] = useState("");
+  // const [preference, setPreference] = useState("");
+  // const [like, setLike] = useState("");
+  // const [dislike, setDislike] = useState("");
+  // const [favorite, setFavorite] = useState("");
+  // const [bigNoNo, setBigNoNo] = useState("");
 
   const classes = useStyles();
+  useEffect(() => {
+    console.log("inside useEffct");
+    fetch("http://localhost:8080/loggedin")
+      .then((response) => response.json())
+      .then((data) => console.log("DATAAA", data));
+  }, []);
+
   return (
     <div className={classes.root}>
       <AppBar position="static">
@@ -47,10 +56,14 @@ const NavBar = () => {
             color="inherit"
             aria-label="menu"
           >
-            <MenuIcon />
+            <Menu>
+              <MenuItem>Profile</MenuItem>
+              <MenuItem>My account</MenuItem>
+              <MenuItem>Logout</MenuItem>
+            </Menu>
           </IconButton>
           <Typography variant="h6" className={classes.title}>
-            USER NAME
+            {user}
           </Typography>
           <Typography variant="h4" className={classes.title}>
             HUNGR
