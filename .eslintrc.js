f22cd4bcdf7a61a0e3d4ec0bb988366b605d6fb4
jackiedouglass <jackie.douglass@icloud.com>
2021-08-12 10:34:44 -0500

    Fixed eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-// module.exports = {
-//   extends: "airbnb",
-//   root: true,
-//   env: {
-//     browser: true,
-//     node: true,
-//     jest: true
-//   },
-//   rules: {
-//     "arrow-parens": "off",
-//     "consistent-return": "off",
-//     "func-names": "off",
-//     "no-console": "off",
-//     "radix": "off",
-//     "react/button-has-type": "off",
-//     "react/destructuring-assignment": "off",
-//     "react/jsx-filename-extension": "off",
-//     "react/prop-types": "off",
-//   },
-// };
-=======
 module.exports = {
   extends: 'airbnb',
   root: true,
@@ -41,4 +19,3 @@ module.exports = {
     'comma-dangle': 'off',
   },
 };
->>>>>>> c2867c0344002f0e84ba9338e78ef728c51d24be
